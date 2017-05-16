88a25b4f62c91db69b41293c5784562fabbe44da
andreygolubkow <andreygolubkow@gmail.com>
2017-05-17 03:13:51 +0700

    Подключил EF

---
+++ CleverHostel/Core/DataBase/StudentContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.DataBase
+{
+    using System;
+    using System.Data.Entity;
+
+    using Core.Model.Student;
+
+    public class StudentContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StudentContext"/> class.
+        /// </summary>
+        public StudentContext()
+            : base("DbConnection")
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
