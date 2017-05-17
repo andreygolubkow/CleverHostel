95d6587d41e9d8b909f0a25f90faae8f1eacd611
andreygolubkow <andreygolubkow@gmail.com>
2017-05-18 04:30:30 +0700

    Добавил контексты данных

---
+++ CleverHostel/Core/DataBase/GroupContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Core.Model.Student;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.DataBase
+{
+    public class GroupContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=hostelData;Trusted_Connection=True;");
+        }
+    }
+}
