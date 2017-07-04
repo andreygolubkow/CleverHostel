cd5d1e05da46de6dcb24707357d37181f238ff7e
andreygolubkow <andreygolubkow@gmail.com>
2017-07-04 11:15:28 +0700

    Создал приложение MVVM

---
+++ CleverHostel/HostelWpfApp/DataHandlerForm.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+using HostelData.DataBase;
+
+using Microsoft.Win32;
+
+namespace HostelWpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для DataHandlerForm.xaml
+    /// </summary>
+    public partial class DataHandlerForm : Window
+    {
+        public DataHandlerForm(HostelContext dataBasContext)
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+                                            {
+                                                Title = "Выберите файл для импорта",
+                                                Multiselect = false,
+                                                CheckFileExists = true,
+                                                CheckPathExists = true,
+                                                DefaultExt = "CSV файл|*.csv",
+                                                Filter = "CSV файл|*.csv"
+                                            };
+            if ( openFileDialog.ShowDialog() == true )
+            {
+                FileNameTextBox.Text = openFileDialog.FileName;
+            }
+
+        }
+    }
+}
